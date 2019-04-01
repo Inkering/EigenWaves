192e39302b21466bf1da4dcfe2dbe943f1458ce1
Dieter Brehm <inkering@gmail.com>
2019-04-01 10:28:29 -0400

    fix typo

--- identify_audio.m
+++ identify_audio.m
@@ -70,7 +70,7 @@ z = U'*(train_vec_ms);
 
 % Run guessing algorithm by finding the image in training that is most
 % similar to the test image and returning the guessed index
-guesses = zeros(size(test_vec,2);
+guesses = zeros(size(test_vec,2));
 for im=1:size(test_vec,2)
     exampleImage = x(:,im);
 %     err = sum(abs(exampleImage-z));
